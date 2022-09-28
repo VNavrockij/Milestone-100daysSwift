475cb6d842e8dcc43bd89f7a23150f95dd2b8139
VNavrockij <vitalik.nav@gmail.com>
2022-09-28 19:11:14 +0300

    add: reuseable cell

--- Milestone-100daysSwift/ViewController.swift
+++ Milestone-100daysSwift/ViewController.swift
@@ -7,25 +7,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     
-    var pictures = [String]()
+    var pictures: [String] = ["FRANCE", "GERMANY", "US", "POLAND", "SPAIN", "NIGERIA", "ITALY", "UK", "MONACCO",  "IRELAND", "ESTONIA"]
+    var picArr: [UIImage] = [#imageLiteral(resourceName: "france"), #imageLiteral(resourceName: "germany"), #imageLiteral(resourceName: "us"), #imageLiteral(resourceName: "monaco"), #imageLiteral(resourceName: "spain"), #imageLiteral(resourceName: "nigeria"), #imageLiteral(resourceName: "italy"), #imageLiteral(resourceName: "uk"), #imageLiteral(resourceName: "poland"), #imageLiteral(resourceName: "ireland"), #imageLiteral(resourceName: "estonia")]
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let fm = FileManager.default
-        let path = Bundle.main.resourcePath!
-        let items = try! fm.contentsOfDirectory(atPath: path)
-        for item in items {
-            if item.hasPrefix("nssl") {
-                pictures.append(item)
-            }
-            
-        }
+        navigationItem.title = "Flags Info"
+        
+//        let fm = FileManager.default
+//        let path = Bundle.main.resourcePath!
+//        let items = try! fm.contentsOfDirectory(atPath: path)
+//        for item in items {
+//            if item.hasPrefix("nssl") {
+//                pictures.append(item)
+//            }
+//
+//        }
+//
+//        print(pictures)
         
         print(pictures)
+                
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        tableView.rowHeight = 150
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        cell.imageView?.image = picArr[indexPath.row]
+        return cell
+
+//        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+//        }
     }
 
    
